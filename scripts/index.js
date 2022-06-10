862c48f9dc453028eb2d4da1a0fbf7a17761859e
Koshin Valery <koshinva1993@gmail.com>
2022-06-10 15:24:48 +0300

    fixed: исправлены замечанию по ревью кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,20 @@
 let windowEditForm = document.querySelector('.edit-form');
 let btnCloseEditForm = document.querySelector('.edit-form__close-icon');
 let btnOpenEditForm = document.querySelector('.profile__button-edit');
-
-btnOpenEditForm.addEventListener('click', openEditForm);
-btnCloseEditForm.addEventListener('click', closeEditForm);
-
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
-let inputName = document.querySelectorAll('.edit-form__input')[0];
-let inputProfession = document.querySelectorAll('.edit-form__input')[1];
+let inputName = document.querySelector('.edit-form__input_value_name');
+let inputProfession = document.querySelector('.edit-form__input_value_profession');
+let formElement = document.querySelector('.edit-form__form');
 
 function openEditForm() {
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
-  windowEditForm.classList.remove('edit-form_hidden');
+  windowEditForm.classList.add('edit-form_opened');
 }
 function closeEditForm() {
-  windowEditForm.classList.add('edit-form_hidden');
+  windowEditForm.classList.remove('edit-form_opened');
 }
-
-let formElement = document.querySelector('.edit-form__form');
-formElement.addEventListener('submit', formSubmitHandler);
-
 function formSubmitHandler(event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
@@ -29,12 +22,6 @@ function formSubmitHandler(event) {
   closeEditForm();
 }
 
-let placeLike = document.querySelectorAll('.place__like');
-placeLike.forEach((element) => element.addEventListener('click', changeLike));
-function changeLike() {
-  if (this.getAttribute('src') === './images/place/like/like-noactive.svg') {
-    this.setAttribute('src', './images/place/like/like-active.svg');
-  } else {
-    this.setAttribute('src', './images/place/like/like-noactive.svg');
-  }
-}
+btnOpenEditForm.addEventListener('click', openEditForm);
+btnCloseEditForm.addEventListener('click', closeEditForm);
+formElement.addEventListener('submit', formSubmitHandler);
