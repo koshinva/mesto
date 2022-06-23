80893a75794f828224d2be926c498fbf721bda2b
Koshin Valery <koshinva1993@gmail.com>
2022-06-23 18:18:52 +0300

    feat: Добавить возможность открытие модального окна изображения

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
-const editFormProfile = document.querySelector('.edit-form_profile');
-const editFormPlace = document.querySelector('.edit-form_place');
+const editFormProfile = document.querySelector('.popup_type_profile');
+const editFormPlace = document.querySelector('.popup_type_place');
+const popupOpenImage = document.querySelector('.popup_type_image');
+const popupImage = document.querySelector('.popup__image');
+const popupImageDescription = document.querySelector('.popup__image-description');
 const btnCloseEditFormProfile = document.querySelector(
-  '.edit-form__close-icon_location_profile'
+  '.popup__close-icon_location_profile'
 );
 const btnCloseEditFormPlace = document.querySelector(
-  '.edit-form__close-icon_location_place'
+  '.popup__close-icon_location_place'
+);
+const btnClosePopupImage = document.querySelector(
+  '.popup__close-icon_location_image'
 );
 const btnOpenEditForm = document.querySelector('.profile__button-edit');
 const btnAddPlace = document.querySelector('.profile__button-add');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const inputName = document.querySelector('.edit-form__input_value_name');
+const inputName = document.querySelector('.popup__input_value_name');
 const inputProfession = document.querySelector(
-  '.edit-form__input_value_profession'
+  '.popup__input_value_profession'
 );
-const inputTitle = document.querySelector('.edit-form__input_value_title');
-const inputLink = document.querySelector('.edit-form__input_value_link');
-const formElementProfile = document.querySelector('.edit-form__form_location_profile');
-const formElementPlace = document.querySelector('.edit-form__form_location_place');
+const inputTitle = document.querySelector('.popup__input_value_title');
+const inputLink = document.querySelector('.popup__input_value_link');
+const formElementProfile = document.querySelector('.popup__form_location_profile');
+const formElementPlace = document.querySelector('.popup__form_location_place');
 const placeTemplate = document.querySelector('#place-template').content;
 const placeSection = document.querySelector('.place');
-
 
 const initialCards = [
   {
@@ -55,6 +60,7 @@ function addElementPlace(card) {
     .cloneNode(true);
   placeElement.querySelector('.place__image').src = card.link;
   placeElement.querySelector('.place__name-city').textContent = card.name;
+  placeElement.querySelector('.place__image').addEventListener('click', openImage);
   placeElement.querySelector('.place__like').addEventListener('click', changeLike);
   placeElement.querySelector('.place__button-remove').addEventListener('click', removePlace);
   placeSection.append(placeElement);
@@ -63,18 +69,26 @@ function addElementPlace(card) {
 function openEditFormProfile() {
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
-  editFormProfile.classList.add('edit-form_opened');
+  editFormProfile.classList.add('popup_opened');
 }
 function openEditFormPlace() {
   inputTitle.value = '';
   inputLink.value = '';
-  editFormPlace.classList.add('edit-form_opened');
+  editFormPlace.classList.add('popup_opened');
+}
+function openImage(event) {
+  popupImage.src = event.target.src;
+  popupImageDescription.textContent = event.target.parentNode.querySelector('.place__name-city').textContent;
+  popupOpenImage.classList.add('popup_opened');
 }
 function closeEditFormProfile() {
-  editFormProfile.classList.remove('edit-form_opened');
+  editFormProfile.classList.remove('popup_opened');
 }
 function closeEditFormPlace() {
-  editFormPlace.classList.remove('edit-form_opened');
+  editFormPlace.classList.remove('popup_opened');
+}
+function closePopapImage() {
+  popupOpenImage.classList.remove('popup_opened');
 }
 function formSubmitHandler(event) {
   event.preventDefault();
@@ -89,6 +103,7 @@ function formSubmitAddPlace(event) {
     .cloneNode(true);
   placeElement.querySelector('.place__name-city').textContent = inputTitle.value;
   placeElement.querySelector('.place__image').src = inputLink.value;
+  placeElement.querySelector('.place__image').addEventListener('click', openImage);
   placeElement.querySelector('.place__like').addEventListener('click', changeLike);
   placeElement.querySelector('.place__button-remove').addEventListener('click', removePlace);
   placeSection.prepend(placeElement);
@@ -101,11 +116,11 @@ function removePlace(event) {
   event.target.parentNode.remove();
 }
 
-
 initialCards.forEach(addElementPlace);
 btnOpenEditForm.addEventListener('click', openEditFormProfile);
 btnAddPlace.addEventListener('click', openEditFormPlace);
 btnCloseEditFormProfile.addEventListener('click', closeEditFormProfile);
 btnCloseEditFormPlace.addEventListener('click', closeEditFormPlace);
+btnClosePopupImage.addEventListener('click', closePopapImage)
 formElementProfile.addEventListener('submit', formSubmitHandler);
 formElementPlace.addEventListener('submit', formSubmitAddPlace);
