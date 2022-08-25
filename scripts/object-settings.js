1122d653c45a6dec736701dbc9847d622066ca69
Koshin Valery <koshinva1993@gmail.com>
2022-08-25 14:33:13 +0300

    Updates

--- scripts/object-settings.js
+++
@@ -1,9 +0,0 @@
-const objectSettings = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_inactive',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popap__input-error_active',
-};
-export default objectSettings;
